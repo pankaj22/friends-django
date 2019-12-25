a6b53554d10e20216dd6bdd261f5d7fc19e6b669
Pankaj Gupta <pankajg@mechmocha.com>
2019-12-26 00:03:08 +0530

    db models and migrations has been added to the project.

--- app/friends/models.py
+++ app/friends/models.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class users(models.Model):
+    name = models.CharField(max_length=32)
+    last_seen_ts = models.DateTimeField()
+    created_at_ts = models.DateTimeField()
+
+
+class friend_relation(models.Model):
+    user_id = models.ForeignKey(users, on_delete=models.CASCADE, related_name='user_id')
+    friend_id = models.ForeignKey(users, on_delete=models.CASCADE, related_name='friend_id')
+    status = models.IntegerField(default=0)
+    created_at_ts = models.DateTimeField()
